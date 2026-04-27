da68399321bac0a52970d53a7c48df9b523ac07d
Kevin-Raj16 <kevinraj592@gmail.com>
2026-04-27 10:55:51 +0530

    data file

---
+++ data.js
@@ -0,0 +1,12 @@
+// Users
+export const users = [
+  { id: 1, name: "Kevin" },
+  { id: 2, name: "Arun" }
+];
+
+// Products
+export const products = [
+  { id: 1, name: "Laptop", price: 50000 },
+  { id: 2, name: "Phone", price: 20000 },
+  { id: 3, name: "Headphones", price: 2000 }
+];
