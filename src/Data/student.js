a4aae6e42a57f2cf12c6c08743f487e67be910a0
Kevin-Raj16 <kevinraj592@gmail.com>
2026-06-10 11:36:27 +0530

    studentlist

---
+++ src/Data/student.js
@@ -0,0 +1,32 @@
+const students = [
+  {
+    id: 1,
+    name: "Kevin Raj",
+    department: "Computer Science",
+    email: "kevin@gmail.com",
+    attendance: "92%"
+  },
+  {
+    id: 2,
+    name: "Arun Kumar",
+    department: "Information Technology",
+    email: "arun@gmail.com",
+    attendance: "88%"
+  },
+  {
+    id: 3,
+    name: "Karthik",
+    department: "ECE",
+    email: "karthik@gmail.com",
+    attendance: "95%"
+  },
+  {
+    id: 4,
+    name: "Vignesh",
+    department: "Mechanical",
+    email: "vignesh@gmail.com",
+    attendance: "85%"
+  }
+];
+
+export default students;
