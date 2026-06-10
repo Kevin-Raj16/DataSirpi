d44c2a83b2ca8f138bdb0adc144c3ae4749b7590
Kevin-Raj16 <kevinraj592@gmail.com>
2026-06-10 11:35:23 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+/**
+ * Footer Component
+ */
+function Footer() {
+
+  return (
+    <footer className="footer">
+      <p>
+        Student Information System
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
