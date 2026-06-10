701c4fe1e4ab58e221f9879ef834a53d803603eb
Kevin-Raj16 <kevinraj592@gmail.com>
2026-06-10 11:33:22 +0530

    header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+/**
+ * Header Component
+ * Displays application title
+ */
+function Header() {
+  return (
+    <header>
+      <h1>Student Information System</h1>
+    </header>
+  );
+}
+
+export default Header;
