26384c155132b3559b9a6732d5c43872f12cbf3e
Kevin-Raj16 <kevinraj592@gmail.com>
2026-06-10 11:37:13 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from "./components/Header";
+import StudentList from "./components/StudentList";
+import Footer from "./components/Footer";
+
+/**
+ * Root Component
+ */
+function App() {
+
+  return (
+    <>
+
+      <Header />
+
+      <StudentList />
+
+      <Footer />
+
+    </>
+  );
+}
+
+export default App;
