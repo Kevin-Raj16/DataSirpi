084309ebf7f8cd36abd1ce86d53e4ad68498da5e
Kevin-Raj16 <kevinraj592@gmail.com>
2026-06-30 13:53:19 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Header from "./components/Header";
+import Dashboard from "./components/Dashboard";
+import Footer from "./components/Footer";
+
+import "./App.css";
+
+/**
+ * Root Component
+ */
+function App() {
+
+    return (
+
+        <>
+
+            <Header />
+
+            <Dashboard />
+
+            <Footer />
+
+        </>
+
+    );
+
+}
+
+export default App;
