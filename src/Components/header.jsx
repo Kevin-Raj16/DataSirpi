7f1882a9f9ff1b322139c8bc267e0eb816d041d7
Kevin-Raj16 <kevinraj592@gmail.com>
2026-06-30 13:48:28 +0530

    Create header.jsx

---
+++ src/Components/header.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+/**
+ * Header Component
+ */
+class Header extends Component {
+
+    render() {
+
+        return (
+
+            <header className="header">
+
+                <h1>📚 Library Management System</h1>
+
+                <p>
+                    React Class Components Demo
+                </p>
+
+            </header>
+
+        );
+
+    }
+
+}
+
+export default Header;
