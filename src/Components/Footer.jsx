22ffc834049c2f2689838bfb89a073fca53e58ba
Kevin-Raj16 <kevinraj592@gmail.com>
2026-06-30 13:48:53 +0530

    Create Footer.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+/**
+ * Footer Component
+ */
+class Footer extends Component {
+
+    render() {
+
+        return (
+
+            <footer className="footer">
+
+                <p>
+                    © 2026 Library Management System
+                </p>
+
+            </footer>
+
+        );
+
+    }
+
+}
+
+export default Footer;
