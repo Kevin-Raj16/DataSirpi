615390aaf6be5f3bc12613e34a07d5ea1cbd693b
Kevin-Raj16 <kevinraj592@gmail.com>
2026-08-12 11:10:14 +0530

    Create product.js

---
+++ src/data/product.js
@@ -0,0 +1,48 @@
+const products = [
+  {
+    id: 1,
+    name: "MacBook Pro M4",
+    category: "Laptop",
+    price: 185000,
+    rating: 4.9,
+    stock: true,
+    quantity: 15,
+    image:
+      "https://images.unsplash.com/photo-1517336714739-489689fd1ca8"
+  },
+  {
+    id: 2,
+    name: "iPhone 16 Pro",
+    category: "Mobile",
+    price: 125000,
+    rating: 4.8,
+    stock: true,
+    quantity: 25,
+    image:
+      "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9"
+  },
+  {
+    id: 3,
+    name: "Sony WH1000XM5",
+    category: "Headphone",
+    price: 32000,
+    rating: 4.7,
+    stock: false,
+    quantity: 0,
+    image:
+      "https://images.unsplash.com/photo-1505740420928-5e560c06d30e"
+  },
+  {
+    id: 4,
+    name: "Apple Watch Ultra",
+    category: "Watch",
+    price: 82000,
+    rating: 4.9,
+    stock: true,
+    quantity: 12,
+    image:
+      "https://images.unsplash.com/photo-1523275335684-37898b6baf30"
+  }
+];
+
+export default products;
